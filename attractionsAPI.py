b62733aadef3049e248a764face34d20e546729b
Alexander Reams <alexander.reams@biola.edu>
2022-07-06 19:44:44 +0000

    second commit

--- attractionsAPI.py
+++ attractionsAPI.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from requests.structures import CaseInsensitiveDict
 
 
 
@@ -25,6 +24,7 @@ from requests.structures import CaseInsensitiveDict
 #Main Function
 GEOAPIFY_APIKEY = 'ccc449b727344041a2a3038f3eb7e098'
 
+
 city_prompt = "Enter the city of where you want to find the attractions of: "
 city = str(input(city_prompt))
 state_prompt = "Enter the city's full state name: "
@@ -34,11 +34,46 @@ if len(state_prompt) == 2:
         state_prompt = str(input("Error: You entered the initals of the state. Please enter the full name of the state: "))
 
 state = str(input(state_prompt))
-location = city + "," + state
 
-url = "https://api.geoapify.com/v2/places?categories=tourism&filter=circle:-73.935242,40.730610,5000" + "&apiKey=ccc449b727344041a2a3038f3eb7e098" 
+
+geocoding_url = "https://api.geoapify.com/v1/geocode/search?city=" + city + "&state=" + state + "&format=json&apiKey=ccc449b727344041a2a3038f3eb7e098"
+resp = requests.get(geocoding_url, headers=headers).json()
+x = resp["results"]
+
+lon = ""
+lat = ""
+
+i = 0
+while i != len(x):
+    coord_finder = x[i]
+
+    for j in coord_finder:
+        if j == 'lon' or j == 'lat':
+            if j == 'lon':
+                lon = coord_finder[j]
+                print(lon)
+            elif j == 'lat':
+                lat = coord_finder[j]
+                print(lat)
+
+    if lon == "" and lat == "":
+        i += 1
+    else:
+        break
+
+radius = "5000"
+places_url = "https://api.geoapify.com/v2/places?categories=tourism&filter=circle:circle:{lon},{lat},{radius}" + "&apiKey=ccc449b727344041a2a3038f3eb7e098" 
 
 categories_prompt = "Enter the name of the category of the attraction you want to find\n[1]Accomodation [2]Ac"
-location_data = requests.get(url).json()
+location_data = requests.get(places_url).json()
+
 print(location_data)
+
+"""
+i = 0
+location_values = location_data.values()
+while i != len(location_values):
+    print(l)
+"""
+
     
