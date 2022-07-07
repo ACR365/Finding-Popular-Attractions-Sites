32a649054841984560d643d091053188c1e03719
Alexander Reams <alex.reams02@gmail.com>
2022-07-07 13:09:03 +0000

    July 7th 6AM Commit

--- attractionsAPI.py
+++ attractionsAPI.py
@@ -3,60 +3,51 @@ import json
 from requests.structures import CaseInsensitiveDict
 import pprint
 import PySimpleGUI as sg
+import gui 
 
 
-class GUI:
-    __categories__ = ["hotels", "restaurants", "attraction-sites",
-                      "museums", "leisure-parks", "heritage"]
-    __geoapify_categories_keymap__ = {
-        "hotels": "accommodation.hotel",
-        "restaurants": "catering",
-        "attraction-sites": "tourism",
-        "museums": "entertainment.museum",
-        "leisure-parks": "leisure.park",
-        "heritage": "heritage",
-        "": ""
-    }
-    geoapify_keys = {}
+def geocoding(gui_info, city, state, category, GEOAPIFY_APIKEY, headers):
+    gui_keys = gui_info.keys()
 
-    @classmethod
-    def setup(cls):
-        layout = [
-            [sg.Column([[sg.Image("back.png", size=(250, 200))]], justification='center')],
-            [sg.Text('Please enter the city, state, and category of destination you are looking for :)',
-                     font=("Lucida", 11))],
-            [sg.Text('City', size=(15, 1), font=("Lucida", 11)), sg.InputText(key='city')],
-            [sg.Text('State (in full)', size=(15, 1), font=("Lucida", 11)), sg.InputText(key='state')],
-            [sg.Text('Category', size=(15, 1), font=("Lucida", 11)),
-             sg.Combo(cls.__categories__, font=("Lucida", 11), key='category')],
-            [sg.Column([[sg.Submit(), sg.Cancel()]], justification='center')]
-        ]
+    x = 0
+    for g in gui_keys:
+        if g == "city":
+            city = gui_info["city"]
+        elif g == "state":
+            state = gui_info["state"]
+        elif g == "category":
+            category = gui_info["category"]
 
-        # Create the window
-        sg.change_look_and_feel('BluePurple')
-        window = sg.Window("Trip Advisor", layout, margins=(100, 80), element_padding=(10,10))
+    geocoding_url = "https://api.geoapify.com/v1/geocode/search?city=" + city + "state=" + state + "&format=json&apiKey=" + GEOAPIFY_APIKEY
 
-        # event
-        event, values = window.read()
+    response = requests.get(geocoding_url, headers=headers).json()
+    geocoding_data = response['results']
 
-        try:
-            cls.geoapify_keys = {
-                "city": values['city'].lower(),
-                "state": values['state'].lower(),
-                "categories": cls.__geoapify_categories_keymap__[values['category']]
-            }
-        except Exception:
-            print("Error")
-        finally:
-            window.close()
+    lon = ""
+    lat = ""
 
-    @classmethod
-    def get_keys(cls):
-        return cls.geoapify_keys
+    i = 0
+    while i != len(geocoding_data):
+        coord_finder = geocoding_data[i]
+    
+        for j in coord_finder:
+            if j == 'lon' or j == 'lat':
+                if j == 'lon':
+                    lon = coord_finder[j]
+                elif j == 'lat':
+                    lat = coord_finder[j]
+
+        if lon == "" and lat == "":
+            i += 1
+        else:
+            break
 
 
+    radius = "16100"
+    places_url = f"https://api.geoapify.com/v2/places?categories={category}&filter=circle:{lon},{lat},{radius}&&apiKey={GEOAPIFY_APIKEY}" 
 
-
+    return places_url
+    
 
 
 
@@ -78,51 +69,23 @@ class GUI:
 
 
 #Main Function
-gui = GUI()
-gui.setup()
-print(gui.get_keys())
-
 GEOAPIFY_APIKEY = 'ccc449b727344041a2a3038f3eb7e098'
 
-"""
-geocoding_url = f"https://api.geoapify.com/v1/geocode/search?city={city}&state={state}&format=json&apiKey={GEOAPIFY_APIKEY}"
+gui = gui.GUI()
+gui.setup()
+gui_info = gui.get_keys()
+
+city = ""
+state = ""
+category = ""
+
 headers = CaseInsensitiveDict()
 headers["Accept"] = "application/json"
 
-response = requests.get(geocoding_url, headers=headers).json()
-geocoding_data = response['results']
+place_url = geocoding(gui_info, city, state, category, GEOAPIFY_APIKEY, headers)
+print(place_url)
 
-lon = ""
-lat = ""
-
-i = 0
-while i != len(geocoding_data):
-    coord_finder = geocoding_data[i]
-    
-    for j in coord_finder:
-        if j == 'lon' or j == 'lat':
-            if j == 'lon':
-                lon = coord_finder[j]
-            elif j == 'lat':
-                lat = coord_finder[j]
-
-    if lon == "" and lat == "":
-        i += 1
-    else:
-        break
-
-print(lon)
-print(lat)
-
-radius = "16100"
-places_url = f"https://api.geoapify.com/v2/places?categories=tourism&filter=circle:{lon},{lat},{radius}&&apiKey={GEOAPIFY_APIKEY}" 
-
-categories_prompt = "Enter the name of the category of the attraction you want to find\n[1]Accomodation [2]Ac"
 location_data = requests.get(places_url).json()
-
-pprint.pprint(location_data)
-
-
 
 i = 0
 location_values = location_data.values()
---
+++ gui.py
@@ -0,0 +1,53 @@
+"""GUI setup class for travel advisor"""
+import PySimpleGUI as sg
+
+
+class GUI:
+    __categories__ = ["hotels", "restaurants", "attraction-sites",
+                      "museums", "leisure-parks", "heritage"]
+    __geoapify_categories_keymap__ = {
+        "hotels": "accommodation.hotel",
+        "restaurants": "catering",
+        "attraction-sites": "tourism",
+        "museums": "entertainment.museum",
+        "leisure-parks": "leisure.park",
+        "heritage": "heritage",
+        "": ""
+    }
+    geoapify_keys = {}
+
+    @classmethod
+    def setup(cls):
+        layout = [
+            [sg.Column([[sg.Image("back.png", size=(250, 200))]], justification='center')],
+            [sg.Text('Please enter the city, state, and category of destination you are looking for :)',
+                     font=("Lucida", 11))],
+            [sg.Text('City', size=(15, 1), font=("Lucida", 11)), sg.InputText(key='city')],
+            [sg.Text('State (in full)', size=(15, 1), font=("Lucida", 11)), sg.InputText(key='state')],
+            [sg.Text('Category', size=(15, 1), font=("Lucida", 11)),
+             sg.Combo(cls.__categories__, font=("Lucida", 11), key='category')],
+            [sg.Column([[sg.Submit(), sg.Cancel()]], justification='center')]
+        ]
+
+        # Create the window
+        sg.change_look_and_feel('BluePurple')
+        window = sg.Window("Trip Advisor", layout, margins=(100, 80), element_padding=(10,10))
+
+        # event
+        event, values = window.read()
+
+        try:
+            cls.geoapify_keys = {
+                "city": values['city'].lower(),
+                "state": values['state'].lower(),
+                "categories": cls.__geoapify_categories_keymap__[values['category']]
+            }
+        except Exception:
+            print("Error")
+        finally:
+            window.close()
+
+    @classmethod
+    def get_keys(cls):
+        return cls.geoapify_keys
+
