08bab701f5a5ef23c30b16e0343c93178056e89e
Alexander Reams <alex.reams02@gmail.com>
2022-07-07 17:53:16 +0000

    Updated with pycodestyle changes

--- attractionsAPI.py
+++ attractionsAPI.py
@@ -3,7 +3,7 @@ import json
 from requests.structures import CaseInsensitiveDict
 import pprint
 import PySimpleGUI as sg
-import gui 
+import gui
 
 
 def geocoding(gui_info, city, state, category, GEOAPIFY_APIKEY, headers):
@@ -18,7 +18,7 @@ def geocoding(gui_info, city, state, category, GEOAPIFY_APIKEY, headers):
         elif g == "categories":
             category = gui_info["categories"]
 
-    geocoding_url = "https://api.geoapify.com/v1/geocode/search?city=" + city + "&state=" + state + "&format=json&apiKey=" + GEOAPIFY_APIKEY
+    geocoding_url = f"https://api.geoapify.com/v1/geocode/search?city={city}&state={state}&format=json&apiKey={GEOAPIFY_APIKEY}
     response = requests.get(geocoding_url, headers=headers).json()
     geocoding_data = response['results']
 
@@ -41,24 +41,23 @@ def geocoding(gui_info, city, state, category, GEOAPIFY_APIKEY, headers):
         else:
             break
 
-
     radius = "16100"
     place_url = f"https://api.geoapify.com/v2/places?categories={category}&filter=circle:{lon},{lat},{radius}&&apiKey={GEOAPIFY_APIKEY}" 
-
     return place_url
 
 
-
-#Main Function
+# Main Function
 GEOAPIFY_APIKEY = 'ccc449b727344041a2a3038f3eb7e098'
 
+
 gui = gui.GUI()
+
+
 gui.setup()
 gui_info = gui.get_keys()
 
 if gui_info == {}:
     raise Exception("Error: Missing one or more fields")
-
 
 city = ""
 state = ""
@@ -75,19 +74,19 @@ l_d = location_data["features"]
 locations = []
 for ld in l_d:
     location = {}
-    location_check = {"name": "", "address": "", "website": ""}
+    location_check = {"name": "", "address": ""}
 
     location_properties = ld["properties"]
 
-    for l in location_properties.keys():
-        if l == "address_line1":
+    for loc in location_properties.keys():
+        if loc == "address_line1":
             value = location_properties[l]
             location["name"] = value
-        elif l == "address_line2":
+        elif loc == "address_line2":
             value = location_properties[l]
             location["address"] = value
 
     locations.append(location.copy())
 
-for l in locations:
-    print (l)
+for location in locations:
+    print(location)
